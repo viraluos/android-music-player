36ad4946324a00afd94cf64dfce3bc5c0923dbc0
Mario semeraro <business@viraluos.com>
2025-04-16 17:03:13 +0200

    .

--- app/src/main/java/com/example/musicplayer/data/database/SongDao.java
+++ app/src/main/java/com/example/musicplayer/data/database/SongDao.java
@@ -1,4 +1,26 @@
 package com.example.musicplayer.data.database;
 
-public class SongDao {
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+// import androidx.room.Delete;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface SongDao {
+
+    @Insert
+    void insert(SongEntity song);
+
+    @Update
+    void update(SongEntity song);
+
+    @Query("SELECT * FROM songs ORDER BY views DESC")
+    List<SongEntity> getAllSongs();
+
+    @Query("SELECT * FROM songs WHERE id = :id")
+    SongEntity getSongById(String id);
+
 }

79e8ee5e59fd1693a7c02589f57f330a369825bf
Mario semeraro <business@viraluos.com>
2025-05-05 21:07:08 +0200

    .

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -4,16 +4,6 @@ import android.os.Bundle;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.musicplayer.data.api.ApiClient;
-import com.example.musicplayer.data.api.SongApi;
-import com.example.musicplayer.data.model.Song;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {

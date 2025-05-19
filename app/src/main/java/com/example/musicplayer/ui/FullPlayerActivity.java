ea1e5230a3cec2a73d558bb258fb483b4bbbbf07
Riccardo <riccardo.marchi2006@gmail.com>
2025-05-19 16:58:08 +0200

    Modificato qualcosa

--- app/src/main/java/com/example/musicplayer/ui/FullPlayerActivity.java
+++ app/src/main/java/com/example/musicplayer/ui/FullPlayerActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.musicplayer.PlayerHelper;
 import com.example.musicplayer.R;
 
 public class FullPlayerActivity extends AppCompatActivity {
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_full);
+
+
     }
 }
